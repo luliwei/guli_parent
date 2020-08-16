2915a78b58cb20ef9fd1832714a618ab0de3b06f
luliwei <47958390+luliwei@users.noreply.github.com>
2020-08-16 21:21:50 +0800

    在线更新

--- service/service_edu/src/test/java/com/atguigu/test/gittest.java
+++ service/service_edu/src/test/java/com/atguigu/test/gittest.java
@@ -8,5 +8,6 @@ public class gittest {
         System.out.println("创建分支dev");
         System.out.println("主干添加");
         System.out.println("分支添加");
+        System.out.println("在线更新");
     }
 }
